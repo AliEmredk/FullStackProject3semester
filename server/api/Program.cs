70c051f3e98815223846348f3b1cc7ccda99e443
AliEmredk <aliemreuzunoglu@gmail.com>
2025-09-27 13:56:28 +0200

    edit api Program.cs ,test database and run api (it is working)

--- server/api/Program.cs
+++ server/api/Program.cs
@@ -1,6 +1,31 @@
+using efscaffold.Entities;
+using Infrastructure.Postgres.Scaffolding;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<MyDbContext>(conf =>
+{
+    conf.UseNpgsql("Server=ep-nameless-scene-agbj7qqp-pooler.c-2.eu-central-1.aws.neon.tech;DB=neondb;UID=neondb_owner;PWD=npg_erGOQ8I3cbkT;SslMode=require");
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", ([FromServices]MyDbContext dbContext) =>
+{
+    var myTodo = new Todo()
+    {
+        Description = "Test",
+        Title = "test title",
+        Id = Guid.NewGuid().ToString(),
+        Isdone = false,
+        Priority = 5
+    };
+    dbContext.Todos.Add(myTodo);
+    dbContext.SaveChanges();
+    var objects = dbContext.Todos.ToList();
+    return objects;
+});
 
 app.Run();
