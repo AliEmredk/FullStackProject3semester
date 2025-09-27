89d5476ce4d097695fd5409c9e9284f035e87887
AliEmredk <aliemreuzunoglu@gmail.com>
2025-09-27 10:56:45 +0200

    add .env file make scaffold.sh to access it via CONN_STR placeholder

--- server/efscaffold/scaffold.sh
+++ server/efscaffold/scaffold.sh
@@ -1,5 +1,11 @@
+#I deleted !/bin/bash if you have time try to find out is it necessary and what exactly does
+set -a
+#this says, it is reading from .env file then will source $CONN_STR placeholder from .env file
+source .env
+set +a
+
 dotnet tool install -g dotnet-ef
-dotnet ef dbcontext scaffold "Server=ep-nameless-scene-agbj7qqp-pooler.c-2.eu-central-1.aws.neon.tech;DB=neondb;UID=neondb_owner;PWD=npg_erGOQ8I3cbkT;SslMode=require" Npgsql.EntityFrameworkCore.PostgreSQL \
+dotnet ef dbcontext scaffold "$CONN_STR" Npgsql.EntityFrameworkCore.PostgreSQL \
     --output-dir ./Entities \
     --context-dir . \
     --context MyDbContext \
