d3a7c740fefa567a72bb9750589751ef52f9db45
AliEmredk <aliemreuzunoglu@gmail.com>
2025-10-05 14:58:20 +0200

    connect client and server then add CORS

--- server/api/Program.cs
+++ server/api/Program.cs
@@ -1,3 +1,4 @@
+using System.Text.Json;
 using api;
 using efscaffold.Entities;
 using Infrastructure.Postgres.Scaffolding;
@@ -9,12 +10,22 @@ var builder = WebApplication.CreateBuilder(args);
 
 var appOptions = builder.Services.AddAppOptions(builder.Configuration);
 
+Console.WriteLine("the app options are: " + JsonSerializer.Serialize(appOptions));
+
 builder.Services.AddDbContext<MyDbContext>(conf =>
 {
     conf.UseNpgsql(appOptions.DbConnectionString);
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
+
+app.UseCors(config => config
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .AllowAnyOrigin()
+    .SetIsOriginAllowed(x => true));
 
 app.MapGet("/", (
     
